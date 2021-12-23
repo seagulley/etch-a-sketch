ce04f66540e7ac325e65579fef91a7c7693b716c
smootbrain <87551908+smootbrain@users.noreply.github.com>
2021-12-23 12:24:13 -0700

    added a select grid size function

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('body');
 
+var gridDimensions = prompt('Enter the grid size!');
+if (gridDimensions >> 100) {
+    gridDimensions = 100;
+};
+
+
 const title = document.createElement('div');
 title.classList.add('title');
-title.textContent = 'Etch-a-Sketch!'
+title.innerText = 'Etch-a-Sketch!'
 
 body.insertBefore(title, body.children[0]);
 
@@ -10,14 +16,20 @@ const container = document.querySelector('.container');
 
 var i = 0;
 var aDiv;
-for (i; i<64; i++) {
+for (i; i<(gridDimensions**2); i++) {
     aDiv = document.createElement('div');
     aDiv.classList.add('square');
+    aDiv.addEventListener('mouseover', changeColor);
     container.appendChild(aDiv);
 };
 
-function changeColor() {
+container.style.cssText = `grid-template-columns: repeat(${gridDimensions}, 1fr); 
+    grid-template-rows: repeat(${gridDimensions}, 1fr)`
+
+
+
+function changeColor(e) {
     this.classList.add('colored')};
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener("mousedown", changeColor));
+// const squares = document.querySelectorAll('.square');
+// squares.forEach(square => square.addEventListener("mousedown", changeColor));
